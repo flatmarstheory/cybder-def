0ab137d8dea031e4bda5d989b4fb05c9cb4067fa
Rai <rai@tutorbuddha.co>
2023-04-01 15:25:58 +0530

    Some changes

---
+++ hello-mars.c
@@ -0,0 +1,10 @@
+// Create a "Hello, Mars!" program that prints the string to the console.
+// Compile: gcc -o hello-mars hello-mars.c
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, Mars! \n");
+    return 0;
+}
